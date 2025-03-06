127cf6bdddb607ed75cf36d9c03264f2d9d328dd
amhore01 <amhore01@gmail.com>
2025-03-06 10:07:37 +0330

    canPlaceShip function completed

--- Answers/BattleShip/src/main/java/BattleShip.java
+++ Answers/BattleShip/src/main/java/BattleShip.java
@@ -117,7 +117,18 @@ public class BattleShip {
       @return true if the ship can be placed at the specified location, false otherwise.
      */
     static boolean canPlaceShip(char[][] grid, int row, int col, int size, boolean horizontal) {
-        //todo
+        if (horizontal) {
+            if (col + size > GRID_SIZE) return false;
+            for (int i = 0; i < size; i++) {
+                if (grid[row][col + i] != '~') return false;
+            }
+        } else {
+            if (row + size > GRID_SIZE) return false;
+            for (int i = 0; i < size; i++) {
+                if (grid[row + i][col] != '~')
+                    return false;
+            }
+        }
         return true;
     }
 
